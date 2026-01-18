551f132e97f609f2a2883af0d89d181652a4dfbe
Nandani Thakur <tnandani868@gmail.com>
2026-01-18 11:02:41 +0545

    Updated 12.c and 34.c

--- 12.c
+++ 12.c
@@ -8,7 +8,7 @@ int main()
     scanf("%d",&num);
     if (num>0)
     {
-        printf("%d is positive");
+        printf("%d is positive",num);
     }
     else if (num<0)
     {
--- 34.c
+++ 34.c
@@ -23,3 +23,33 @@ int main()
 
     return 0;
 }
+
+
+
+/*
+12345
+1234
+123
+12
+1
+*/
+
+/*
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    // Outer loop for rows
+    for(i = 5; i >= 1; i--) {
+        // Inner loop to print numbers in each row
+        for(j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+        printf("\n"); // move to next row
+    }
+
+    return 0;
+}
+    */
+
